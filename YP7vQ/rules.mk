96293dc23475bffa317ca4f2a87ba03039d593e8
Ryan Camaratta <ryancnap@gmail.com>
2024-12-26 19:12:47 -0500

    Update rules.mk

--- YP7vQ/rules.mk
+++ YP7vQ/rules.mk
@@ -2,6 +2,7 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
+COMBO_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 MOUSEKEY_ENABLE = no
 ORYX_ENABLE = yes
