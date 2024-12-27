665b4ac4760546e9ac33ee7177a1b7730f9f4d0d
Ryan Camaratta <ryancnap@gmail.com>
2024-12-26 19:24:38 -0500

    Update keymap.c
    trying combos

--- YP7vQ/keymap.c
+++ YP7vQ/keymap.c
@@ -24,6 +24,18 @@ void matrix_scan_user(void) {
   achordion_task();
 }
 
+//combo
+enum combos {
+  GM_ENT
+};
+
+const uint16_t PROGMEM gm_combo[] = {KC_G, KC_M, COMBO_END};
+
+combo_t key_combos[] = {
+  [GM_ENT] = COMBO(gm_combo, KC_ENTER),
+};
+//end combo
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     TD(DANCE_0),    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
